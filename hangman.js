59f1dda654a9f98d9ab0096e3b73e3c9ae408638
alexrivas05 <alex12rivas34@gmail.com>
2026-04-10 23:50:03 -0400

    Added dialogue for winning and losing, code that doesn’t allow guesses before the word is chosen or after you’ve guessed correctly or failed, and code that prevents the same guesses.

--- hangman.js
+++ hangman.js
@@ -5,6 +5,15 @@ var guesses = "";
 var guessCount;
 const MAX_GUESSES = 6;
 
+let isWordComplete = function(){
+    for (let i = 0; i < word.length; i++) {
+        if (guesses.indexOf(word.charAt(i)) < 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 let newGame = function(){
     guessCount = MAX_GUESSES;
     let randomIndex = parseInt(Math.random()*POSSIBLE_WORDS.length);
@@ -35,18 +44,31 @@ let updatePage = function(){
 
     let image = document.getElementById("hangmanpic");
     image.src = `images/hangman${guessCount}.gif`;
+
+    if(guessCount===0){
+        guessArea.textContent = "Sorry, You Failed! Try Again!";
+    }
+    else if (isWordComplete()){
+        guessArea.textContent = "You Won!"
+    }
 }
 
 let guessLetter = function(){
     let input = document.getElementById("guess");
     let letter = input.value;
     letter = letter.toLowerCase();
-    if(word.indexOf(letter) < 0){
-        guessCount--;
-
+    
+    if (word !== "" && guessCount > 0 && !isWordComplete()){
+    if (guesses.indexOf(letter) < 0) {
+      
+        if(word.indexOf(letter) < 0) {
+            guessCount--;
+        }
+        guesses+=letter;
     }
-    guesses+=letter;
-    input.value = "";
+    
     updatePage();
+}
 
+    input.value = "";
 }
